bf2e9a0ada23f5a38ee5999b7022290ff81f7b38
nikhilkarnwal <nikhilkarnwal@yahoo.com>
2016-11-26 00:58:41 +0530

    default code containing macros and headers
    default code containing macros and headers

---
+++ default_code.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define rep(i,a,n) for(int i=a;i<n;i++)
+#define rrep(i,a,n) for(int i=n-1;i>=a;i--)
+#define pb push_back
+#define rb pop_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+
+typedef vector<int> VI;
+typedef long long ll;
+typedef pair<int,int> PII;
+
+const ll mod = 1000000007;
