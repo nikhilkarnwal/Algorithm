aa7763049f8e06655237162e3ce32878355f38d1
nikhilkarnwal <nikhilkarnwal@yahoo.com>
2016-11-27 11:37:54 +0530

    different application of binary search.
    Different application of binary search

--- bs_find_partition.cpp
+++ bs_find_partition.cpp
@@ -30,6 +30,7 @@ typedef pair<int,int> PII;
 
 const ll mod = 1000000007;
 
+//Find partition in increasing then decreasing sequence
 int bsFindPartition(VI ar){
     int i=0,j=sz(ar)-1;
     while(i<j){
@@ -50,12 +51,46 @@ int bsFindPartition(VI ar){
     return -1;
 }
 
+//find element in the rotated sorted sequence
+int bsFindInRotated(VI ar,int k){
+    int i=0,j=sz(ar)-1;
+    while(i<=j){
+        int mid=i+(j-i)/2;
+        if(ar[mid]==k){
+            return mid;
+        }
+        if(ar[mid]<ar[i]&&ar[mid]<ar[j]){
+            if(k>=ar[i]||k<ar[mid]){
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        else if(ar[mid]>ar[i]&&ar[mid]>ar[j]){
+            if(k>ar[mid]||k<=ar[j]){
+                i=mid+1;
+            }
+            else{
+                j=mid-1;
+            }
+        }
+        else{
+            if(k>ar[mid])
+                i=mid+1;
+            else{
+                j=mid-1;
+            }
+        }
+    }
+    return -1;
+}
 int main(){
     VI v;
-    v.pb(2);
-    v.pb(5);
-    //v.pb(6);
-    v.pb(4);
+    v.pb(6);
+    v.pb(7);
     v.pb(3);
-    cout<<bsFindPartition(v);
+    v.pb(4);
+    v.pb(5);
+    cout<<bsFindInRotated(v,8);
 }
